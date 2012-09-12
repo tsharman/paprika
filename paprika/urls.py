0b1a3e99a6d23cc34b0d439d578177fa73fa0760
Torehan Sharman <torehan7@gmail.com>
2012-09-12 19:36:43 +0000

    new logo

--- paprika/urls.py
+++ paprika/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
-
+from django.contrib import admin
+admin.autodiscover()
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'paprika.views.home'),
     url(r'^login/', 'paprika.views.login'),
     # Examples:
