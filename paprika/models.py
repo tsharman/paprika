27b681aad63e2ce088a5713037dff7b0e126d3bd
prashanth <sadpr@umich.edu>
2012-09-13 21:57:40 +0000

    first thing of models

--- paprika/models.py
+++ paprika/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
+
+class Flow(models.Model):
+  flow_name = models.CharField(max_length=100)
+
 class Stage(models.Model):
   title = models.CharField(max_length=100)
   description = models.CharField(max_length=200)
+  stage_num = models.IntegerField()
+  flow = models.ForeignKey(Flow)
+
+
+
