14307060d78a578ce0662e3a4f066289997d3dc6
Torehan Sharman <torehan7@gmail.com>
2012-09-19 16:46:59 +0000

    tracking page and early submission of add order form

--- paprika/business/views.py
+++ paprika/business/views.py
@@ -2,12 +2,21 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
+from django.http import HttpResponse, HttpResponseRedirect
 
 @login_required(login_url='/')
 def orders(request):
-  from paprika.models import Order
-  orders = Order.objects.filter(merchant=request.user)
-  return render_to_response('orders.html', {'orders' : orders }, context_instance=RequestContext(request))
+  if request.method == 'GET':
+    from paprika.models import Order, BusinessProfile
+    orders = Order.objects.filter(merchant=request.user)
+    return render_to_response('orders.html', {'user' : request.user, 'orders' : orders}, context_instance=RequestContext(request))
+  elif request.method == 'POST':
+    from paprika.business.forms import OrderForm
+    order = OrderForm(request.POST)
+    if order.is_valid:
+      return HttpResponse("not valid!")
+    else:
+      return HttpResponse('added order!') 
 
 @login_required(login_url='/')
 def flows(request):
--- paprika/static/js/orders.js
+++ paprika/static/js/orders.js
@@ -16,13 +16,11 @@ $(document).ready(function() {
   //hover over stages
   $(".stage").hover(function() {
     $(this).children(".status_bar").css("background-color", "#54c56f");
-    $(this).children(".stage_title").slideUp(100, function() {
-      $(this).siblings(".start_here").slideDown(100);
-    });
+    $(this).children(".stage_title").hide();
+    $(this).children(".start_here").show();
   }, function() {
     $(this).children(".status_bar").css("background-color", "#888");
-    $(this).children(".start_here").slideUp(100, function() {
-      $(this).siblings(".stage_title").slideDown(100);
-    });
+    $(this).children(".start_here").hide();
+    $(this).children(".stage_title").show();
   });
 });
