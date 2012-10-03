decdcceffa3f4cb91a76660ad34fadb40bdb47f2
Torehan Sharman <torehan7@gmail.com>
2012-10-03 18:59:18 +0000

    adding new form elements for stages when creating a new flow

--- paprika/static/js/flows.js
+++ paprika/static/js/flows.js
@@ -13,14 +13,16 @@ $(document).ready(function() {
     });
   });
 
-  //hover over stages
-  $(".stage").hover(function() {
-    $(this).children(".status_bar").css("background-color", "#54c56f");
-    $(this).children(".stage_title").hide();
-    $(this).children(".start_here").show();
-  }, function() {
-    $(this).children(".status_bar").css("background-color", "#888");
-    $(this).children(".start_here").hide();
-    $(this).children(".stage_title").show();
+  // add a stage
+  $("#add_stage_btn").click(function() {
+    var container = $("#stage_form_list");
+
+    var stage_form = $("<div class='stage_form'></div>");
+ 
+    stage_form.append("<input type='text' class='title' placeholder='Stage title' />"); 
+    stage_form.append("<input type='text' class='description' placeholder='Stage description' />");
+    
+    container.append(stage_form);
   });
+
 });
