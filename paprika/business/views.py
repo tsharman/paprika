47b0ac090a98d93a32f0ae517186441eecc654d8
Torehan Sharman <torehan7@gmail.com>
2012-10-03 01:29:55 +0000

    add_flow_dialog setup

--- paprika/business/views.py
+++ paprika/business/views.py
@@ -5,12 +5,12 @@ from django.template import RequestContext
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect
 
 @login_required(login_url='/')
-def orders(request, order_filter):
+def orders(request, order_state):
   if request.method == 'GET':
     from paprika.models import Order, BusinessProfile
 
     # filtering through different order states
-    orders = Order.objects.filter(merchant=request.user, state=order_filter)
+    orders = Order.objects.filter(merchant=request.user, state=order_state)
 
     return render_to_response('orders.html', {'user' : request.user, 'orders' : orders}, context_instance=RequestContext(request))
   elif request.method == 'POST':
---
+++ paprika/static/js/flows.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  //add order btn
+  $("#add_flow_btn").click(function() {
+    $("#dialogs").fadeIn(200, function() {
+      $("#add_flow_dialog").fadeIn(200);
+    });
+  });
+
+  //close the dialog
+  $(".close_dialog").click(function() {
+    $(this).parent().fadeOut(200, function() {
+      $("#dialogs").fadeOut(200);
+    });
+  });
+
+  //hover over stages
+  $(".stage").hover(function() {
+    $(this).children(".status_bar").css("background-color", "#54c56f");
+    $(this).children(".stage_title").hide();
+    $(this).children(".start_here").show();
+  }, function() {
+    $(this).children(".status_bar").css("background-color", "#888");
+    $(this).children(".start_here").hide();
+    $(this).children(".stage_title").show();
+  });
+});
