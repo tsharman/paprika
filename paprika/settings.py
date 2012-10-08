20c759b1ef3123cd3f7d5f635fca538862aae0c6
Torehan Sharman <torehan7@gmail.com>
2012-10-08 01:43:48 +0000

    fixing wierd admin user deletion problem

--- paprika/settings.py
+++ paprika/settings.py
@@ -136,7 +136,6 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'paprika',
     'south',
-    'tastypie',
     'compressor',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
