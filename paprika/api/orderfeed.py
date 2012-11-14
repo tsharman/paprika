e123552d019a8e8a966da4c9802bbaa4314402b0
Evan Koschik <ekoschik@umich.edu>
2012-11-14 18:41:07 +0000

    Revert "changed spacing back to 2 for orders and orderfeed"
    This reverts commit e4c5d63c6dc08cdbffd173aa19feb16737f29c23.

--- paprika/api/orderfeed.py
+++ paprika/api/orderfeed.py
@@ -1,24 +1,25 @@
 from tastypie.resources import ModelResource
 from tastypie import fields
 from paprika.models import Order, FeedEntry
+from tastypie.authorization import Authorization
 from tastypie.authorization import DjangoAuthorization
 from paprika.authentication import TwoLeggedOAuthAuthentication
 
 
 class FeedProxy(ModelResource):
-  class Meta:
-    queryset = FeedEntry.objects.all()
-    fields = ['body', 'time_entered']
-    include_resource_uri = False
-
+    class Meta:
+        queryset = FeedEntry.objects.all()
+        fields = ['body', 'time_entered']
+        include_resource_uri = False
+        
 
 class OrderFeedResource(ModelResource):
-  feeds = fields.ToManyField(FeedProxy, 'feeds', full=True)
-  class Meta:
-    queryset = Order.objects.all()
-    detail_allowed_methods = ['get']
-    list_allowed_methods = []
-    include_resource_uri = False
-    resource_name = 'feed'
-    authorization = DjangoAuthorization()
-    authentication = TwoLeggedOAuthAuthentication()
+    class Meta:
+        queryset = Order.objects.all()
+        detail_allowed_methods = ['get']
+        list_allowed_methods = []
+        include_resource_uri = False
+        resource_name = 'feed'
+        authorization = DjangoAuthorization()
+        authentication = TwoLeggedOAuthAuthentication()
+    feeds = fields.ToManyField(FeedProxy, 'feeds', full=True)
