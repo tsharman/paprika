88dbba0a1be1a5989ee60801b677ac4c21975d8b
Torehan <torehan7@gmail.com>
2012-12-03 02:42:00 +0000

    return WWW-Authenticate when no authenticated

--- paprika/utils/basicauth.py
+++ paprika/utils/basicauth.py
@@ -6,19 +6,20 @@ from django.views.decorators.csrf import csrf_exempt
 def authenticated(f):
     @csrf_exempt
     def wrap(request, *args, **kwargs):
-        
         try:
             auth = request.META['HTTP_AUTHORIZATION'].split()
             uname, passwd = base64.b64decode(auth[1]).split(':')
             user = authenticate(username=uname, password=passwd)
         except:
-            response = HttpResponse("Unauthorized") 
+            response = HttpResponse() 
             response.status_code = 401
+            response['WWW-Authenticate'] = 'Basic realm=""'
             return response
 
         if user is None:
             response = HttpResponse("Unauthorized")
             response.status_code = 401
+            response['WWW-Authenticate'] = 'Basic realm=""'
             return response
         return f(request, *args, **kwargs)
     wrap.__doc__=f.__doc__
